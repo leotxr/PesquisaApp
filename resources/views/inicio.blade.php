a325fc762f8595a626a30acb299c604b33c530a1
leotxr <leonardof15@hotmail.com>
2023-06-30 17:28:14 -0300

    toast notification

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,9 +7,13 @@
                 <div class="grid max-w-xl grid-cols-1 sm:grid-cols-1 justify-items-center">
                     <h1 class="py-5 text-5xl font-bold text-blue-700">Sua opinião é muito importante para nós!</h1>
                     <a href="{{ url('welcome') }}" type="button"
-                        class="h-16 p-2 text-xl font-bold text-white bg-blue-800 border-2 border-none rounded-md hover:bg-blue-900 btn w-96">Iniciar Pesquisa</a>
+                        class="h-16 p-2 text-xl font-bold text-white bg-blue-800 border-2 border-none rounded-md hover:bg-blue-900 btn w-96">Iniciar
+                        Pesquisa</a>
 
                 </div>
 
             </div>
+        </div>
+    </div>
+
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,13 +24,16 @@
         
         @if($errors->any())
         @foreach($errors->all() as $error)
-        <div class="absolute shadow-lg alert alert-warning">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-6 h-6 stroke-current" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                </svg>
-                <span>{{$error}}</span>
+        <div id="toast-danger" class="absolute flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow right-5 bottom-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Error icon</span>
             </div>
+            <div class="ml-3 text-sm font-normal">{{$error}}</div>
+            <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button>
         </div>
         @endforeach
         @endif
