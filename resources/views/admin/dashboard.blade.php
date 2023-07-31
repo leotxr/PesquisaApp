fe5efd92026f742aabcf407a7ddb40799f56aaa1
leotxr <leonardof15@hotmail.com>
2023-07-31 07:59:31 -0300

    background

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-2 text-center">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-sm bg-base-200 sm:rounded-lg">
+            <div class="overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 border-b bg-base border-base-200">
                     <div class="justify-center">
 
