6858865445c2ecdea77c8f78a00f9942f54ae1f1
leotxr <leonardof15@hotmail.com>
2024-02-01 09:07:52 -0300

    Correção no relatório por setores.

--- app/Http/Livewire/Reports/SectorReport.php
+++ app/Http/Livewire/Reports/SectorReport.php
@@ -15,9 +15,23 @@ class SectorReport extends Component
     public function mount()
     {
 
-
+        //dd(DB::connection('sqlsrv')->table('FATURA')
+        //    ->whereBetween('DATA', ['2024-02-01', '2024-02-01'])
+        //    ->join('SETORES', 'SETORES.SETORID', '=', 'FATURA.SETORID')
+        //    ->whereNotIn('SETORES.SETORID', [6, 8, 11, 12, 15, 16, 17, 19])
+        //    ->where('SETORES.DESCRICAO', '=', 'RAIOSX')
+        //    ->select(DB::raw('COUNT(DISTINCT (FATURA.REQUISICAOID)) AS TOTAL'))->first());
     }
 
+    public function getFaturas($start_date, $end_date)
+    {
+       return DB::connection('sqlsrv')->table('FATURA')
+            ->whereBetween('DATA', [$start_date, $end_date])
+            ->join('SETORES', 'SETORES.SETORID', '=', 'FATURA.SETORID')
+            ->whereNotIn('SETORES.SETORID', [6, 8, 11, 12, 15, 16, 17, 19])
+            ->where('SETORES.DESCRICAO', '=', 'RAIOSX')
+            ->select(DB::raw('COUNT(DISTINCT (FATURA.REQUISICAOID)) AS TOTAL'))->first();
+    }
     public function search()
     {
         $this->reset('faturas');
@@ -29,12 +43,7 @@ class SectorReport extends Component
 
         foreach ($arr as $a) {
             $count = Fatura::whereBetween('created_at', [$this->start_date . ' 00:00:00', $this->end_date . ' 23:59:59'])->where('setor', $a)->count();
-            $this->faturas[] = (object)['setor' => $a, 'total' => $count, 'x_clinic' => DB::connection('sqlsrv')->table('FATURA')
-                ->whereBetween('DATA', [$this->start_date, $this->end_date])
-                ->join('SETORES', 'SETORES.SETORID', '=', 'FATURA.SETORID')
-                ->whereNotIn('SETORES.SETORID', [6,8,11,12,15,16,17,19])
-                ->where('SETORES.DESCRICAO', '=', $a)
-                ->count()];
+            $this->faturas[] = (object)['setor' => $a, 'total' => $count, 'x_clinic' => $this->getFaturas($this->start_date, $this->end_date)->TOTAL];
         }
 
 
