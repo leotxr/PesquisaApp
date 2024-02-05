f7e875fd350b688b9d807bde59c5228f4f102532
leotxr <leonardof15@hotmail.com>
2024-02-05 09:07:52 -0300

    correção ao puxar tabela com dados do pivot

--- app/Http/Livewire/Reports/EmployeeReport.php
+++ app/Http/Livewire/Reports/EmployeeReport.php
@@ -41,8 +41,9 @@ class EmployeeReport extends Component
 
         foreach (Employee::role('tecnico')->get() as $employee)
             $this->technicians[] = (object)['name' => $employee->name,
-                'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
-                'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'tec')->count(),
+                'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
 
         foreach (Employee::role('recepcionista usg')->get() as $employee)
             $this->usg_receptionists[] = (object)['name' => $employee->name,
@@ -51,8 +52,9 @@ class EmployeeReport extends Component
 
         foreach (Employee::role('enfermeira')->get() as $employee)
             $this->nurses[] = (object)['name' => $employee->name,
-                'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'enf')->count(),
                 'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+
 
         $this->render();
     }
