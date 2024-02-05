fcf49c0ffe57d1e562a159716888b65413b71fbf
leotxr <leonardof15@hotmail.com>
2024-02-05 15:07:57 -0300

    relatório de satisfação por setor.

--- resources/views/livewire/reports/reports-index.blade.php
+++ resources/views/livewire/reports/reports-index.blade.php
@@ -4,7 +4,8 @@
     { id: 1, label: 'Quantidade de Avaliações e Atendimentos no X-Clinic por Funcionário', link: '{{route("reports.report-by-employee")}}' },
     { id: 2, label: 'Quantidade de Avaliações e Atendimentos no X-Clinic por Setor', link: '{{route("reports.report-by-sector")}}' },
     { id: 3, label: 'Resumo de Avaliações por período', link: '{{route("rel-geral")}}' },
-    { id: 4, label: 'Relatório de comentários por período', link: '{{route("rel-coment")}}' }
+    { id: 4, label: 'Relatório de comentários por período', link: '{{route("rel-coment")}}' },
+    { id: 5, label: 'Relatório de satisfação por setor', link: '{{route("reports.ratings-by-sector")}}' }
 ]}">
             <template x-for="report in reports" :key="report.id">
                 <li><a class="space-x-2 cursor-pointer hover:border-b hover:border-b-primary" type="button" :href="report.link" wire:navigate><span x-text="report.id" class="font-bold"></span>:<span x-text="report.label"></span></a></li>
