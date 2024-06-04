01db4828945a9317097e7fa91f800beeb4323f20
leotxr <leonardof15@hotmail.com>
2024-06-04 14:18:04 -0300

    correção de bug na trait para buscar por setor angiologia

--- app/Http/Livewire/Reports/EmployeeReport.php
+++ app/Http/Livewire/Reports/EmployeeReport.php
@@ -48,7 +48,7 @@ class EmployeeReport extends Component
         foreach (Employee::role('recepcionista usg')->get() as $employee)
             $this->usg_receptionists[] = (object)['name' => $employee->name,
                 'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
-                'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+                'x_clinic_count' => $this->compareServiceUSG([5, 10, 22], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
 
         foreach (Employee::role('enfermeira')->get() as $employee)
             $this->nurses[] = (object)['name' => $employee->name,
