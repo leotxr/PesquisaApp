025c374d4f7a309b767b38fbd0d4cde9176382c5
Leonardo <leonardof15@hotmail.com>
2025-02-05 19:51:06 -0300

    debug:data

--- app/Http/Controllers/EmployeeReportController.php
+++ app/Http/Controllers/EmployeeReportController.php
@@ -41,6 +41,36 @@ class EmployeeReportController extends Controller
     public function getRelatorioFuncionarios(Request $request)
     {
         $data = $request;
+        $this->start_date = $data->dataInicio;
+        $this->end_date = $data->dataFim;
+
+        return $this->start_date;
+
+        foreach (Employee::role('recepcionista')->get() as $employee)
+        {
+            $this->receptionists[] = (object)['name' => $employee->name,
+                'count' => $employee->ratings->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'rec')->count(),
+                'x_clinic_count' => $this->compareServiceRec($this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+            $this->rec_agendamento[] = (object)['name'=> $employee->name,
+            'count'=> 1,
+            'x_clinic_count' => 1];
+        }
+
+        foreach (Employee::role('tecnico')->get() as $employee)
+            $this->technicians[] = (object)['name' => $employee->name,
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'tec')->count(),
+                'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
+
+        foreach (Employee::role('recepcionista usg')->get() as $employee)
+            $this->usg_receptionists[] = (object)['name' => $employee->name,
+                'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
+                'x_clinic_count' => $this->compareServiceUSG([5, 10, 22], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+
+        foreach (Employee::role('enfermeira')->get() as $employee)
+            $this->nurses[] = (object)['name' => $employee->name,
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'enf')->count(),
+                'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
 
         return $data;
     }
