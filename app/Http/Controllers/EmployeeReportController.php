578850edcac35a2928640a07c8949d07fe211529
Leonardo <leonardof15@hotmail.com>
2025-02-05 20:28:32 -0300

    tratando dados

--- app/Http/Controllers/EmployeeReportController.php
+++ app/Http/Controllers/EmployeeReportController.php
@@ -43,8 +43,7 @@ class EmployeeReportController extends Controller
         $data = $request;
         $this->start_date = $request->dataInicio;
         $this->end_date = $data->dataFim;
-
-        dd($this->start_date);
+        $retorno = [];
 
         foreach (Employee::role('recepcionista')->get() as $employee)
         {
@@ -57,21 +56,34 @@ class EmployeeReportController extends Controller
         }
 
         foreach (Employee::role('tecnico')->get() as $employee)
+        {
             $this->technicians[] = (object)['name' => $employee->name,
                 'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'tec')->count(),
                 'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
+        }
 
         foreach (Employee::role('recepcionista usg')->get() as $employee)
+        {
             $this->usg_receptionists[] = (object)['name' => $employee->name,
                 'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
                 'x_clinic_count' => $this->compareServiceUSG([5, 10, 22], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+        }
 
         foreach (Employee::role('enfermeira')->get() as $employee)
+        {
             $this->nurses[] = (object)['name' => $employee->name,
                 'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'enf')->count(),
                 'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+        }
 
-        return $data;
+        $retorno['recepcionistas'] = $this->receptionists;
+        $retorno['recep_agendamento'] = $this->rec_agendamento;
+        $retorno['tecnicos'] = $this->technicians;
+        $retorno['usg'] = $this->usg_receptionists;
+        $retorno['enfermeiras'] = $this->nurses;
+        
+        return json_encode($retorno);
+
     }
 }
