74a2f2c020f7f2f5573989e0268f8b75368908f2
Leonardo <leonardof15@hotmail.com>
2025-02-06 19:04:54 -0300

    subindo busca agd

--- app/Http/Livewire/Reports/EmployeeReport.php
+++ app/Http/Livewire/Reports/EmployeeReport.php
@@ -18,6 +18,7 @@ class EmployeeReport extends Component
     public $nurses = [];
     public $start_date;
     public $end_date;
+    public $adg_receptionists = [];
 
     public function mount()
     {
@@ -35,26 +36,39 @@ class EmployeeReport extends Component
     {
         $this->reset('receptionists', 'technicians', 'usg_receptionists', 'nurses');
         foreach (Employee::role('recepcionista')->get() as $employee)
-            $this->receptionists[] = (object)['name' => $employee->name,
-                'count' => $employee->ratings->whereBetween('data_req', [$this->start_date, $this->end_date])->count(),
-                'x_clinic_count' => $this->compareServiceRec($this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+            $this->receptionists[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->ratings->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'rec')->count(),
+                'x_clinic_count' => $this->compareServiceRec($this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
 
         foreach (Employee::role('tecnico')->get() as $employee)
-            $this->technicians[] = (object)['name' => $employee->name,
+            $this->technicians[] = (object)[
+                'name' => $employee->name,
                 'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'tec')->count(),
                 'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
 
         foreach (Employee::role('recepcionista usg')->get() as $employee)
-            $this->usg_receptionists[] = (object)['name' => $employee->name,
+            $this->usg_receptionists[] = (object)[
+                'name' => $employee->name,
                 'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
-                'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+                'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
 
         foreach (Employee::role('enfermeira')->get() as $employee)
-            $this->nurses[] = (object)['name' => $employee->name,
+            $this->nurses[] = (object)[
+                'name' => $employee->name,
                 'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'enf')->count(),
-                'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL];
+                'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
 
+        foreach (Employee::role('recepcionista')->get() as $employee)
+            $this->adg_receptionists[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->ratings->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'agd')->count(),
+                'x_clinic_count' => $this->compareServiceRec($this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
 
         $this->render();
     }
