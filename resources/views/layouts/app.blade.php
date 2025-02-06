d2f305d7c71d9a5124fe7530bed4928cc266c74e
Leonardo <leonardof15@hotmail.com>
2025-02-06 17:52:11 -0300

    importando jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
-    <script
-  src="https://code.jquery.com/jquery-3.7.1.js"
-  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-  crossorigin="anonymous"></script>
 
     @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
