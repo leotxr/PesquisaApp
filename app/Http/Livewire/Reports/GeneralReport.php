2d05cb9172c73d662899c44792c167cafef3daaa
Leonardo <leonardof15@hotmail.com>
2025-02-10 20:47:26 -0300

    debug

--- app/Http/Livewire/Reports/GeneralReport.php
+++ app/Http/Livewire/Reports/GeneralReport.php
@@ -28,6 +28,7 @@ class GeneralReport extends Component
         ->whereNotNull('employee_rating.rate')
         ->where('model_has_roles.role_id', $role_id)
         ->where('employee_rating.role', 'agd')
+        ->whereNotNull('atend_rate')
         ->select(DB::raw('ratings.recep_rate as recep_rate'));
 
         return $agd;
