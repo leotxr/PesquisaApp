9efbf927cebda6f777d350dbf9a5043e47dcc235
Leonardo <leonardof15@hotmail.com>
2025-02-10 22:12:36 -0300

    debug

--- app/Http/Livewire/Reports/SatisfactionByEmployee.php
+++ app/Http/Livewire/Reports/SatisfactionByEmployee.php
@@ -33,35 +33,45 @@ class SatisfactionByEmployee extends Component
 
     }
 
+    private function calcSatisfacao($total, $count)
+    {
+
+    }
+
     public function search()
     {
         $this->reset('receptionists', 'technicians', 'usg_receptionists', 'nurses', 'agd_receptionists', 'agendamentos');
         foreach (Employee::role('recepcionista')->get() as $employee)
+        {
+            $ratings = $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'rec');
             $this->receptionists[] = (object)[
                 'name' => $employee->name,
-                'count' => $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'rec')->count(),
-                'x_clinic_count' => $this->compareServiceRec($this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
-            ];
+                'count' => $ratings->count(),
+                'otimo' => $ratings->where('recep_rate', '>', 3)->count(),
+                'regular' => $ratings->where('recep_rate', '=', 3)->count(),
+                'ruim' => $ratings->where('recep_rate', '<', 3)->count()];
+        }
+            
 
         foreach (Employee::role('tecnico')->get() as $employee)
             $this->technicians[] = (object)[
                 'name' => $employee->name,
                 'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'tec')->count(),
-                'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+                'satisfacao' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
 
         foreach (Employee::role('recepcionista usg')->get() as $employee)
             $this->usg_receptionists[] = (object)[
                 'name' => $employee->name,
                 'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
-                'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+                'satisfacao' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
 
         foreach (Employee::role('enfermeira')->get() as $employee)
             $this->nurses[] = (object)[
                 'name' => $employee->name,
                 'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'enf')->count(),
-                'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+                'satisfacao' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
 
         foreach (Employee::role('recepcionista')->get() as $employee)
@@ -75,7 +85,7 @@ class SatisfactionByEmployee extends Component
             $this->agd_receptionists[] = (object)[
                 'name' => $employee->name,
                 'count' => $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'agd')->count(),
-                'x_clinic_count' => $this->compareServiceRecAgd($dados)
+                'satisfacao' => $this->compareServiceRecAgd($dados)
             ];
         }
 
@@ -94,7 +104,7 @@ class SatisfactionByEmployee extends Component
             $this->agendamentos[] = (object)[
                 'name' => $employee->name,
                 'count' => $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'agd')->count(),
-                'x_clinic_count' => $this->compareServiceRecAgd($dados)
+                'satisfacao' => $this->compareServiceRecAgd($dados)
             ];
         }
 
