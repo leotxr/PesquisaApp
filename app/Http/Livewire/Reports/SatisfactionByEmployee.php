a8c3d5e229966672f41605c7ea38dd9a0e97a425
Leonardo <leonardof15@hotmail.com>
2025-02-10 22:01:40 -0300

    testando subindo

---
+++ app/Http/Livewire/Reports/SatisfactionByEmployee.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Livewire\Reports;
+
+use App\Models\Employee;
+use App\Traits\XClinicTraits;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+class SatisfactionByEmployee extends Component
+{
+
+    use XClinicTraits;
+
+    public $technicians = [];
+    public $receptionists = [];
+    public $usg_receptionists = [];
+    public $nurses = [];
+    public $start_date;
+    public $end_date;
+    public $agd_receptionists = [];
+    public $agendamentos = [];
+
+    public function mount()
+    {
+        // $recepcionistas = Employee::role('recepcionista')->get();
+        // $tecnicos = Employee::role('tecnico')->get();
+        // $enfermeiras = Employee::role('enfermeira')->get();
+
+        //dd($this->compareServiceNurse([4,9], '2024-01-30', '2024-01-30', 79)[0]->TOTAL);
+        //dd($this->compareServiceRec( '2024-01-01', '2024-01-24', 8)[0]->TOTAL);
+
+
+    }
+
+    public function search()
+    {
+        $this->reset('receptionists', 'technicians', 'usg_receptionists', 'nurses', 'agd_receptionists', 'agendamentos');
+        foreach (Employee::role('recepcionista')->get() as $employee)
+            $this->receptionists[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'rec')->count(),
+                'x_clinic_count' => $this->compareServiceRec($this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
+
+        foreach (Employee::role('tecnico')->get() as $employee)
+            $this->technicians[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'tec')->count(),
+                'x_clinic_count' => $this->compareServiceTech([1, 2, 3, 4, 9, 13, 18, 20, 21], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
+
+        foreach (Employee::role('recepcionista usg')->get() as $employee)
+            $this->usg_receptionists[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
+                'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
+
+        foreach (Employee::role('enfermeira')->get() as $employee)
+            $this->nurses[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->where('role', 'enf')->count(),
+                'x_clinic_count' => $this->compareServiceNurse([4, 9], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
+            ];
+
+        foreach (Employee::role('recepcionista')->get() as $employee)
+        {
+            $dados = [
+                'dataInicio' => $this->start_date,
+                'dataFim' => $this->end_date,
+                'xClinicId' => $employee->x_clinic_id
+            ];
+
+            $this->agd_receptionists[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'agd')->count(),
+                'x_clinic_count' => $this->compareServiceRecAgd($dados)
+            ];
+        }
+
+        foreach (Employee::role('agendamento')
+        ->whereDoesntHave('roles', function ($query) {
+            $query->where('name', 'recepcionista');
+        })
+        ->get() as $employee)
+        {
+            $dados = [
+                'dataInicio' => $this->start_date,
+                'dataFim' => $this->end_date,
+                'xClinicId' => $employee->x_clinic_id
+            ];
+
+            $this->agendamentos[] = (object)[
+                'name' => $employee->name,
+                'count' => $employee->ratings()->whereBetween('data_req', [$this->start_date, $this->end_date])->where('role', 'agd')->count(),
+                'x_clinic_count' => $this->compareServiceRecAgd($dados)
+            ];
+        }
+
+
+        $this->render();
+    }
+
+    public function render()
+    {
+        return view('livewire.reports.employee-report');
+    }
+}
---
+++ resources/views/livewire/reports/satisfaction-by-employee.blade.php
@@ -0,0 +1,66 @@
+<div>
+    <div wire:loading>
+        @livewire('templates.loading-screen')
+    </div>
+    <form wire:submit="search">
+        <div class="p-2 bg-white shadow sm:p-4 sm:rounded-lg">
+            <x-accordion>
+                <x-slot name="title">
+                    <div class="flex justify-start mx-2 font-bold text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"/>
+                        </svg>
+                        <h1>Filtros</h1>
+                    </div>
+                </x-slot>
+                <x-slot name="content">
+                    <div class="max-w-full">
+                        <div class="grid content-center grid-cols-2 gap-4 sm:grid-cols-5">
+                            <div>
+                                <label for="start_date"
+                                       class="text-sm font-light text-gray-900 label">Data
+                                    inicial</label>
+                                <input type="date" wire:model='start_date' id="start_date"
+                                       class="border-gray-300 input">
+                            </div>
+                            <div>
+                                <label for="end_date"
+                                       class="text-sm font-light text-gray-900 label">Data Final</label>
+                                <input type="date" wire:model='end_date' id="end_date"
+                                       class="border-gray-300 input">
+                            </div>
+                            <div>
+                                <label for="submit"
+                                       class="text-sm font-light text-gray-900 label">Gerar
+                                    relatório</label>
+                                <x-primary-button id="submit" type="submit">
+                                    <x-icon name="refresh" class="w-5 h-5"></x-icon>
+                                    <span>Buscar</span>
+                                </x-primary-button>
+                            </div>
+                            @if($start_date)
+                                <div>
+                                    <label for="export"
+                                           class="text-sm font-light text-gray-900 label">Exportar
+                                    </label>
+                                    <x-primary-button id="export" type="button" class="bg-green-800"
+                                                      wire:click="export">
+                                        <x-icon name="table" class="w-5 h-5"></x-icon>
+                                        <span>Exportar</span>
+                                    </x-primary-button>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </x-slot>
+            </x-accordion>
+        </div>
+    </form>
+    <div>
+        @include('admin.tables.table-satisfaction')
+    </div>
+
+
+</div>
