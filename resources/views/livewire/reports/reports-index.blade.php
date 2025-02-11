83dd64262045bb103c7e3dce3730c1f862bb6045
Leonardo <leonardof15@hotmail.com>
2025-02-11 12:50:08 -0300

    nova rota

--- resources/views/livewire/reports/reports-index.blade.php
+++ resources/views/livewire/reports/reports-index.blade.php
@@ -1,11 +1,13 @@
 <div>
+    <!--teste-->
     <div class="text-left">
         <ul x-data="{ reports: [
     { id: 1, label: 'Quantidade de Avaliações e Atendimentos no X-Clinic por Funcionário', link: '{{route("reports.report-by-employee")}}' },
     { id: 2, label: 'Quantidade de Avaliações e Atendimentos no X-Clinic por Setor', link: '{{route("reports.report-by-sector")}}' },
     { id: 3, label: 'Resumo de Avaliações por período', link: '{{route("rel-geral")}}' },
     { id: 4, label: 'Relatório de comentários por período', link: '{{route("rel-coment")}}' },
-    { id: 5, label: 'Relatório de satisfação por setor', link: '{{route("reports.ratings-by-sector")}}' }
+    { id: 5, label: 'Relatório de satisfação por setor', link: '{{route("reports.ratings-by-sector")}}' },
+    { id: 6, label: 'Relatório de satisfação por funcionário', link: '{{route("reports.satisfaction-by-employee")}}' }
 ]}">
             <template x-for="report in reports" :key="report.id">
                 <li><a class="space-x-2 cursor-pointer hover:border-b hover:border-b-primary" type="button" :href="report.link" wire:navigate><span x-text="report.id" class="font-bold"></span>:<span x-text="report.label"></span></a></li>
