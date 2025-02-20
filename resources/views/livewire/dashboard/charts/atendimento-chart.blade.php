3330f8bc1923f3fec27b425bb49834974c657908
Leonardo <leonardof15@hotmail.com>
2025-02-20 20:47:54 -0300

    fix layout

--- resources/views/livewire/dashboard/charts/atendimento-chart.blade.php
+++ resources/views/livewire/dashboard/charts/atendimento-chart.blade.php
@@ -1,6 +1,6 @@
 <div class="py-6 mx-auto space-y-6 max-w-full sm:py-6">
     <div class="px-4 py-6 bg-white rounded-lg shadow-md dark:bg-gray-800" wire:ignore>
-        <h1>Pesquisas por setor</h1>
+        <h1>Pesquisas por setor de Atendimento e Agendamento</h1>
         <span class="text-xs font-light text-gray-500">Exibe o total de avaliações recebidas ordenadas por setor no mês atual.</span>
         <div class="h-24 bg-white" id="chartAtendimento">
 
