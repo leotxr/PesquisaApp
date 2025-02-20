c227e39ff2e7da6a5413d361d7d04024b687efa2
Leonardo <leonardof15@hotmail.com>
2025-02-20 19:54:26 -0300

    fix layout

--- resources/views/livewire/dashboard/charts/dashboard-chart.blade.php
+++ resources/views/livewire/dashboard/charts/dashboard-chart.blade.php
@@ -1,7 +1,7 @@
 <div class="py-6 mx-auto space-y-6 max-w-full sm:py-6">
     <div class="px-4 py-6 bg-white rounded-lg shadow-md dark:bg-gray-800" wire:ignore>
-        <h1>Pesquisas por setor</h1>
-        <span class="text-xs font-light text-gray-500">Exibe o total de avaliações recebidas ordenadas por setor no mês atual.</span>
+        <h1>Pesquisas por dia</h1>
+        <span class="text-xs font-light text-gray-500">Exibe o total de pesquisas em cada dia, exceto os dias em que não houveram atendimento.</span>
         <div class="h-24 bg-white" id="chartDays">
 
         </div>
