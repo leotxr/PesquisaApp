6cdc3c929375cb2c49cc30e98b5a3d1dbe301b21
Leonardo <leonardof15@hotmail.com>
2025-02-20 19:42:35 -0300

    remove debug

--- app/Http/Livewire/Dashboard/Charts/SectorsChart.php
+++ app/Http/Livewire/Dashboard/Charts/SectorsChart.php
@@ -41,8 +41,6 @@ class SectorsChart extends Component
             ->where('setor', '=', $sector->setor)
             ->count();
         }
-
-        dd($sector_count);
     }
 
     public function render()
