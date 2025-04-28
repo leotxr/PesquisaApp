b9b2d28ec10409b96a33b15b8057b316d1ff9943
Leonardo <leonardof15@hotmail.com>
2025-04-28 18:06:01 -0300

    fix: ajustes no layout

--- resources/views/fim.blade.php
+++ resources/views/fim.blade.php
@@ -1,30 +1,34 @@
 <x-rating-layout>
     <div class="pt-2">
-        <a type="button" href="/"><x-secondary-button type="button">Nova
+        <a type="button" class="absolute" href="/"><x-secondary-button type="button">Nova
                 pesquisa</x-secondary-button></a>
     </div>
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="text-center">
-            <div class="grid justify-items-center">
-                {{-- <img src="{{ URL::asset('image/LOGO_ULTRIMAGEM_VERTICAL.png') }}" class="py-8" width="350px" height="350px"> --}}
+    <div class="h-screen hero">
 
-                <div class="pt-2 text-center">
-                    <h1 class="text-3xl font-bold">Nos avalie também no</h1>
-                    <x-icon name="google-logo" class="w-36 mx-auto" fill="#0a33a3"></x-icon>
-                </div>
+        <div class="text-center hero-content">
+            <div class="max-w-full max-h-full">
+                <div class="grid justify-items-center">
+                    {{-- <img src="{{ URL::asset('image/LOGO_ULTRIMAGEM_VERTICAL.png') }}" class="py-8" width="350px" height="350px"> --}}
 
-                <x-icon name="qrcode-google" class="w-64 h-64 my-4" fill="#0a33a3"></x-icon>
-                <x-icon name="five-stars" solid class="w-64 my-4" fill="#0a33a3"></x-icon>
+                    <div class="pt-1 text-center">
+                        <h1 class="text-2xl font-bold">Nos avalie também no</h1>
+                        <x-icon name="google-logo" class="w-32 mx-auto" fill="#0a33a3"></x-icon>
+                    </div>
 
-                <div class="pt-2">
-                    <h1 class="text-3xl font-bold">A Ultrimagem agradece sua avaliação!</h1>
-                </div>
-                <div class="pt-2">
-                    <h1 class="text-xl font-bold">Iniciando nova pesquisa em <span id="mostrador"></span> segundos</h1>
+                    <x-icon name="qrcode-google" class="w-52 h-52 my-4" fill="#0a33a3"></x-icon>
+                    <x-icon name="five-stars" solid class="w-52 my-4" fill="#0a33a3"></x-icon>
+
+                    <div class="pt-1">
+                        <h1 class="text-2xl font-bold">A Ultrimagem agradece sua avaliação!</h1>
+                    </div>
+                    <div class="pt-1">
+                        <h1 class="text-lg font-bold">Iniciando nova pesquisa em <span id="mostrador"></span> segundos
+                        </h1>
+                    </div>
+
                 </div>
 
             </div>
-
         </div>
     </div>
 </x-rating-layout>
