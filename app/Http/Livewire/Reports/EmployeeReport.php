b5a0cb58f205fb1cf706bfef45597afc2e230af8
leotxr <leonardof15@hotmail.com>
2025-10-11 11:50:23 -0300

    debug

--- app/Http/Livewire/Reports/EmployeeReport.php
+++ app/Http/Livewire/Reports/EmployeeReport.php
@@ -53,13 +53,12 @@ class EmployeeReport extends Component
         }
 
         $usgs = Employee::role('recepcionista usg')->get();
-        dd($usgs);
 
         foreach ($usgs as $employee)
         {
             $this->usg_receptionists[] = (object)[
                 'name' => $employee->name,
-                'count' => $employee->faturas->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
+                'count' => $employee->faturas()->whereBetween('fatura_data', [$this->start_date, $this->end_date])->count(),
                 'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
         }
