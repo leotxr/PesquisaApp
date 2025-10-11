ab50561e50a5f08f70c4acd4c1f545dbb034edca
leotxr <leonardof15@hotmail.com>
2025-10-11 11:08:48 -0300

    debug

--- app/Http/Livewire/Reports/EmployeeReport.php
+++ app/Http/Livewire/Reports/EmployeeReport.php
@@ -80,6 +80,12 @@ class EmployeeReport extends Component
             ];
         }
 
+        $debug = Employee::role('agendamento')
+        ->whereDoesntHave('roles', function ($query) {
+            $query->where('name', 'recepcionista');
+        })
+        ->get();
+        dd($debug);
         foreach (Employee::role('agendamento')
         ->whereDoesntHave('roles', function ($query) {
             $query->where('name', 'recepcionista');
