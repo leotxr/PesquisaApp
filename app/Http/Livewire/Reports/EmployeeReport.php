bd865a1032e8a47892becc7c9259bb400679dcdc
leotxr <leonardof15@hotmail.com>
2025-10-11 10:58:55 -0300

    REMOVENDO DEBUG

--- app/Http/Livewire/Reports/EmployeeReport.php
+++ app/Http/Livewire/Reports/EmployeeReport.php
@@ -58,8 +58,6 @@ class EmployeeReport extends Component
                 'x_clinic_count' => $this->compareServiceUSG([5, 10], $this->start_date, $this->end_date, $employee->x_clinic_id)[0]->TOTAL
             ];
 
-             dd($this->usg_receptionists);exit;
-
         foreach (Employee::role('enfermeira')->get() as $employee)
             $this->nurses[] = (object)[
                 'name' => $employee->name,
